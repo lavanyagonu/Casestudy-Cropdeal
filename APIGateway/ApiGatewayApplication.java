a5af633486ad244b408de9971c7532e6676c622c
lavanyagonu <93313865+lavanyagonu@users.noreply.github.com>
2021-12-14 17:42:46 +0530

    Add files via upload

---
+++ APIGateway/ApiGatewayApplication.java
@@ -0,0 +1,25 @@
+package com.casestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+@EnableEurekaClient
+@SpringBootApplication
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+	@Bean
+	public RestTemplate template()
+	{
+		return new RestTemplate();
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
